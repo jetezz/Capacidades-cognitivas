a371d0b65b765659af9a5126ad83950d1b534507
jetezz <cuadrabc2015@gmail.com>
2021-01-21 12:56:48 +0100

    menu inicial v0.1

---
+++ New Unity Project/Assets/script/menu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void botonEjercicios()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void botonUsuarios()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void botonEstadisticas()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void botonAtras()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
