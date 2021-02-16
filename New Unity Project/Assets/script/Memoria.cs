fdb248cd2cf9fca87683da7391ba99c682e0947c
jetezz <cuadrabc2015@gmail.com>
2021-02-16 13:59:12 +0100

    nivel memoria 3 y reescalado

--- New Unity Project/Assets/script/Memoria.cs
+++ New Unity Project/Assets/script/Memoria.cs
@@ -11,6 +11,7 @@ public class Memoria : MonoBehaviour
     private GameObject managerEjercicios;
     bool respuesta = true;
     private AudioSource fuenteDeAudio;
+    public GameObject imagenRespuesta;
 
 
     public int contadorFases=0;
@@ -44,13 +45,17 @@ public class Memoria : MonoBehaviour
     public GameObject panelNivel2;
     public GameObject botonesNivel2;
 
+    ////Nivel3
+    public GameObject panelNivel3;
+    public GameObject botonesNivel3;
+
     void Start()
     {
         fuenteDeAudio = GetComponent<AudioSource>();
         fasesTotales = new List<fases>();
 
         managerEjercicios = GameObject.FindWithTag("MEje");
-       
+        
 
         if (managerEjercicios.GetComponent<ManagerEjercicios>().nivel == 1)
         {
@@ -456,8 +461,11 @@ public class Memoria : MonoBehaviour
         {
             string aux = fasesTotales[contadorFases].preguntas[fasesTotales[contadorFases].contador];
             panelNivel1.transform.GetChild(0).GetComponent<Text>().text = aux;
-            fuenteDeAudio.clip = fasesTotales[contadorFases].audios[fasesTotales[contadorFases].contador];
-            fuenteDeAudio.Play();
+            if (fasesTotales[contadorFases].audios.Count > fasesTotales[contadorFases].contador)
+            {
+                fuenteDeAudio.clip = fasesTotales[contadorFases].audios[fasesTotales[contadorFases].contador];
+                fuenteDeAudio.Play();
+            }
 
         }
         else
@@ -696,87 +704,333 @@ public class Memoria : MonoBehaviour
 
     void generarBotones()
     {
+
+        GameObject boton1;
+        GameObject boton2;
+        GameObject boton3;
+        GameObject boton4;
+
+        if (nivel == 2)
+        {
+            boton1 = botonesNivel2.transform.GetChild(0).gameObject;
+            boton2 = botonesNivel2.transform.GetChild(1).gameObject;
+            boton3 = botonesNivel2.transform.GetChild(2).gameObject;
+            boton4 = botonesNivel2.transform.GetChild(3).gameObject;
+        }
+        else
+        {
+            boton1 = botonesNivel3.transform.GetChild(0).gameObject;
+            boton2 = botonesNivel3.transform.GetChild(1).gameObject;
+            boton3 = botonesNivel3.transform.GetChild(2).gameObject;
+            boton4 = botonesNivel3.transform.GetChild(3).gameObject;
+        }
+       
+
         if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 0)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Azul";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "rojo";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "Azul";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "Rojo";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "Marron";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "Blanco";
+
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 1)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Verde";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "Marron";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "Verde";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "Marron";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "Amarillo";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "Violeta";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 2)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Blanco";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "Naranja";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "Blanco";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "Naranja";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "Negro";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "Rojo";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 3)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Amarillo";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "Negro";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "Amarillo";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "Negro";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "Azul";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "Verde";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 4)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "1";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "3";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "1";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "3";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "2";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "4";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 5)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "2";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "4";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "2";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "4";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "1";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "5";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 6)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "3";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "5";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "3";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "5";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "1";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "6";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 7)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "4";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "6";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "4";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "6";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "3";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "1";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 8)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Soleado";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "Nublado";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "Soleado";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "Nublado";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "Nieve";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "Viento";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 9)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Cisne y vaca";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "Perros y visne";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "Cisne y vaca";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "Perros y visne";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "Vaca y perro";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "Vaca";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 10)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Izquierda";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "Abajo";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "Izquierda";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "Abajo";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "Derecha";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "Arriba";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 11)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Derecha";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "Arriba";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "Derecha";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "Arriba";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "Centro";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "Izquierda";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 12)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Rallas";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "Circulos";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "Rallas";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "Circulos";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "Lunares";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "Cuadrados";
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 13)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "En la puerta";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "En la pared";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "En la puerta";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "En la pared";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "En la mesa";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "En el suelo";
+
         }
         else if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].pregunta == 14)
         {
-            botonesNivel2.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "Debajo de la mesa";
-            botonesNivel2.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = "Debajo del sofá";
+            boton1.transform.GetChild(0).GetComponent<Text>().text = "Debajo de la mesa";
+            boton2.transform.GetChild(0).GetComponent<Text>().text = "Debajo del sofá";
+            boton3.transform.GetChild(0).GetComponent<Text>().text = "Al lado de la puerta";
+            boton4.transform.GetChild(0).GetComponent<Text>().text = "No hay alfombra";
         }
 
     }
+
+    
+    /// Nivel3
+   
     void nivel3()
     {
+        fases fase1 = new fases();
+        fases fase2 = new fases();
+        fases fase3 = new fases();
+
+        fase1.imagen = granja;
+        fase1.preguntas.Add("¿Cuántos arboles hay?");
+        fase1.soluciones.Add(new Soluciones(4, 3));
+        fase1.preguntas.Add("De qué color es la casa?");
+        fase1.soluciones.Add(new Soluciones(0, 2));
+        fase1.preguntas.Add("Cuántas manzanas hay en el arbol?");
+        fase1.soluciones.Add(new Soluciones(5, 4));
+        fase1.preguntas.Add("De qué color es la barca?");
+        fase1.soluciones.Add(new Soluciones(1, 2));
+        fase1.preguntas.Add("Cómo está el dia?");
+        fase1.soluciones.Add(new Soluciones(8, 2));
+        fase1.preguntas.Add("Qué animales hay?");
+        fase1.soluciones.Add(new Soluciones(9, 1));
+
+        fase2.imagen = habitacion;
+        fase2.preguntas.Add("¿De qué color son las sabanas?");
+        fase2.soluciones.Add(new Soluciones(3, 3));
+        fase2.preguntas.Add("¿Dónde esta el espejo?");
+        fase2.soluciones.Add(new Soluciones(11, 4));
+        fase2.preguntas.Add("De qué color es la alfombra?");
+        fase2.soluciones.Add(new Soluciones(1, 3));
+        fase2.preguntas.Add("¿De qué color es el suelo?");
+        fase2.soluciones.Add(new Soluciones(2, 4));
+        fase2.preguntas.Add("¿Cuántos cuadros hay?");
+        fase2.soluciones.Add(new Soluciones(5, 1));
+        fase2.preguntas.Add("¿Cómo son los cojines?");
+        fase2.soluciones.Add(new Soluciones(12, 1));
+
+        fase3.imagen = salon;
+        fase3.preguntas.Add("¿Dónde esta la tele?");
+        fase3.soluciones.Add(new Soluciones(10, 3));
+        fase3.preguntas.Add("¿De qué color es el sofa?");
+        fase3.soluciones.Add(new Soluciones(2, 4));
+        fase3.preguntas.Add("¿Dónde esta la percha?");
+        fase3.soluciones.Add(new Soluciones(13, 1));
+        fase3.preguntas.Add("¿De qué color es el telefono?");
+        fase3.soluciones.Add(new Soluciones(3, 2));
+        fase3.preguntas.Add("¿Dónde esta la alfombra?");
+        fase3.soluciones.Add(new Soluciones(14, 1));
+        fase3.preguntas.Add("¿De qué color es la lampara?");
+        fase3.soluciones.Add(new Soluciones(1, 3));
+        fase3.preguntas.Add("¿Cuántos cojines hay?");
+        fase3.soluciones.Add(new Soluciones(5, 1));
+
+        fasesTotales.Add(fase1);
+        fasesTotales.Add(fase2);
+        fasesTotales.Add(fase3);
+
+        panelNivel3.SetActive(true);
+        panelNivel3.transform.GetChild(1).GetComponent<Image>().sprite = granja;
+        tiempo = 0;
+    }
+
+
+
+    public void boton1Nivel3()
+    {
+               
+        if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].solucion == 1)
+        {
+            puntos++;
+            respuesta = true;
+        }
+        else
+        {
+            respuesta = false;
+        }
+        tiempo = 3;
+        fasesTotales[contadorFases].contador++;
+        comprobarFinal3();
+    }
+   
+    public void boton2Nivel3()
+    {
+        
+        if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].solucion == 2)
+        {
+            puntos++;
+            respuesta = true;
+        }
+        else
+        {
+            respuesta = false;
+        }
+        tiempo = 3;
+        fasesTotales[contadorFases].contador++;
+        comprobarFinal3();
+    }
+    public void boton3Nivel3()
+    {
+       
+        if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].solucion == 3)
+        {
+            puntos++;
+            respuesta = true;
+
+        }
+        else
+        {
+            respuesta = false;
+        }
+        tiempo = 3;
+        fasesTotales[contadorFases].contador++;
+        comprobarFinal3();
+    }
+    public void boton4Nivel3()
+    {
+       
+        if (fasesTotales[contadorFases].soluciones[fasesTotales[contadorFases].contador].solucion == 4)
+        {
+            puntos++;
+            respuesta = true;
+
+        }
+        else
+        {
+            respuesta = false;
+        }
+        tiempo = 3;
+        fasesTotales[contadorFases].contador++;
+        comprobarFinal3();
+    }
+
+    void comprobarFinal3()
+    {
+        if (fasesTotales[contadorFases].contador == fasesTotales[contadorFases].preguntas.Count)
+        {
+            contadorFases++;
+            if (contadorFases == fasesTotales.Count)
+            {
+                panelFin.SetActive(true);
+                panelFin.transform.GetChild(3).GetComponent<Text>().text = puntos.ToString();
+                panelFin.transform.GetChild(4).GetComponent<Text>().text = "Los puntos maximos de esta prueba son 19 ";
+
+                if (puntos == 19)
+                {
+                    panelFin.transform.GetChild(5).GetComponent<Text>().text = "Mejora de nivel en memoria a nivel 4";
+                    managerEjercicios.GetComponent<ManagerEjercicios>().usuario.memoria(4);
+                    GameObject managerUsuario = GameObject.FindWithTag("MUsu");
+                    managerUsuario.GetComponent<ManagerUsuario>().guardarUsuarios();
+                }
+                else
+                {
+                    panelFin.transform.GetChild(5).GetComponent<Text>().text = "Se mantiene el nivel 3 en memoria";
+                }
+            }
+            else
+            {
+                panelNivel3.transform.GetChild(1).GetComponent<Image>().sprite = fasesTotales[contadorFases].imagen;
+                botonesNivel3.SetActive(false);
+                panelNivel3.transform.GetChild(0).GetComponent<Text>().text = "Memoriza esta imagen y dale al boton empezar";
+            }
+        }
+        else
+        {
+            siguientePreguntaNivel3();
+        }
+    }
+
+    public void botonEmpezar3()
+    {
+        siguientePreguntaNivel3();
+    }
+
+    public void siguientePreguntaNivel3()
+    {
+
+        botonesNivel3.SetActive(true);
+        generarBotones();
+
+        string aux = fasesTotales[contadorFases].preguntas[fasesTotales[contadorFases].contador];
+        panelNivel3.transform.GetChild(0).GetComponent<Text>().text = aux;
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
     void nivel4()
     {
 
@@ -784,10 +1038,9 @@ public class Memoria : MonoBehaviour
 
     public void Update()
     {       
-        if (nivel == 1 || nivel==2)
-        {
+        
             updateNivel1();
-        }
+        
     }
 
     void updateNivel1()
@@ -797,19 +1050,19 @@ public class Memoria : MonoBehaviour
         {
             if (respuesta)
             {
-                canvas.transform.GetChild(6).gameObject.SetActive(true);
-                canvas.transform.GetChild(6).GetComponent<Image>().sprite = tick;
+                imagenRespuesta.SetActive(true);
+                imagenRespuesta.GetComponent<Image>().sprite = tick;
             }
             else
             {
-                canvas.transform.GetChild(6).gameObject.SetActive(true);
-                canvas.transform.GetChild(6).GetComponent<Image>().sprite = cruz;
+                imagenRespuesta.gameObject.SetActive(true);
+                imagenRespuesta.GetComponent<Image>().sprite = cruz;
             }
 
         }
         else
         {
-            canvas.transform.GetChild(6).gameObject.SetActive(false);
+            imagenRespuesta.SetActive(false);
         }
         
     }
